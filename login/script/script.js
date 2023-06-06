d3e9d3dc4dc238dad3e998d0495930067793f44b
Caio Almeida Carvalho Lima <caioalmeidaacl@gmail.com>
2023-06-06 12:10:53 -0300

    Adding new functions

--- login/script/script.js
+++ login/script/script.js
@@ -10,20 +10,29 @@ let usuarios = {
 }
 
 // PRIMEIRA PAGINA
-function checkLogin(){ 
-    let campoLogin = document.getElementById("login-usuario").value
-    let campoSenha = document.getElementById("senha-usuario").value
-    let dadosLogin = usuarios[campoLogin]
-    
-    if(dadosLogin && campoSenha === dadosLogin.senha) {
-        localStorage.setItem('login', campoLogin)
-        localStorage.setItem('cargo', dadosLogin.cargo)
-        window.location.href = "/sistema/autenticacao.html"
-    } else {
-        alert('Login ou senha incorretos')
-    }   
-   
-}
+document.addEventListener("click", function() {
+    function checkLogin(){ 
+        let campoLogin = document.getElementById("login-usuario").value
+        let campoSenha = document.getElementById("senha-usuario").value
+        let register = document.getElementById("cadastro")
+        let dadosLogin = usuarios[campoLogin]
+
+        if(dadosLogin){
+            if(dadosLogin && campoSenha === dadosLogin.senha) {
+                localStorage.setItem('login', campoLogin)
+                localStorage.setItem('cargo', dadosLogin.cargo)
+                window.location.href = "/sistema/autenticacao.html"
+            }  else {
+                alert('Login ou senha incorretos')
+            }   
+        } else { 
+            alert('Usuário não existe')
+            register.hidden = false
+        }
+    }
+    let verify = document.getElementById("button")
+    verify.onclick = checkLogin
+})
 
 
 // Já autenticado
@@ -38,16 +47,35 @@ function updateUser() {
 
 
 // Area administrativa 
-
-function adm() {
-    let cargo = localStorage.getItem('cargo')
-
-    
-    if(cargo === 'Usuario') {
-        alert('Você não tem cargo para acessar')
-        window.location.href = "#"
-    } else if(cargo === 'ADM'){
-        window.location.href = "../sistema/adm.html"
+document.addEventListener("click", function(){
+    function adm() {
+        let cargo = localStorage.getItem('cargo')
+        
+        if(cargo === 'Usuario') {
+            alert('Você não tem cargo para acessar')
+            window.location.href = "#"
+        } else if(cargo === 'ADM'){
+            window.location.href = "../sistema/adm.html"
+        }
     }
+    let area = document.getElementById("adm")
+    area.onclick = adm
+})    
 
-}
+
+// Links
+document.addEventListener("click", function(){ 
+    function getOut() {
+        window.location.href = '/index.html'
+    }
+    
+    function home() {
+        window.location.href = '/sistema/autenticacao.html'
+    }
+    
+    let leave = document.getElementById("leave")
+    leave.onclick = getOut
+    
+    let aut = document.getElementById("home")
+    aut.onclick = home
+})
